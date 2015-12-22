211065edcb6899cbadce2f977b40c4f99eee5267
Aaron Smith <asmith1810@gmail.com>
2015-12-22 15:29:01 -0800

    Exports file and sample request code

--- aaron/app.js
+++ aaron/app.js
@@ -1,1 +1,35 @@
 var http = require('http');
+var creds = require('./creds');
+
+var order = {
+  'account': creds.account,
+  'venue': creds.venue,
+  'symbol': creds.stock,
+  'price': 25000,
+  'qty': 100,
+  'direction': "buy",
+  'orderType': "limit"
+};
+
+var options = {
+  host: creds.baseUrl,
+  path: '/venues/' + creds.venue + '/stocks/' + creds.stock + '/orders',
+  method: 'POST',
+  headers: {'X-Starfighter-Authorization': creds.apiToken}
+};
+
+var callback = function(response) {
+  var str = '';
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    console.log(str);
+  });
+};
+
+var req = http.request(options, callback);
+// This is the data we are posting, it needs to be a string or a buffer
+req.write(JSON.stringify(order));
+req.end();
---
+++ exports.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiToken: 'FOOBAR',
+  venue: 'FOOBAR',
+  stock: 'FOOBAR',
+  baseUrl: 'https://api.stockfighter.io/ob/api',
+  account: 'FOOBAR'
+};
+
+
