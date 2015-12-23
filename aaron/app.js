f843dde35ef631765ce3620815e9365e71a21332
Aaron Smith <asmith1810@gmail.com>
2015-12-22 22:27:20 -0800

    Poll for ask price and request to fill at the ask price.

--- aaron/app.js
+++ aaron/app.js
@@ -3,8 +3,9 @@ var creds = require('./exports.js');
 var sleep = require('sleep');
 var Promise = require('promise');
 
-var goal = 100000;
-var orderSize = 200;
+var goal = 99999;
+var price = 7893;
+var quantity = 50;
 
 var buyOptions = {
   host: creds.baseUrl,
@@ -13,11 +14,11 @@ var buyOptions = {
   headers: {'X-Starfighter-Authorization': creds.apiToken}
 };
 
-// var getPriceOptions = {
-//   host: creds.baseUrl,
-//   path: '/ob/api/venues/' + creds.venue + '/stocks/' + creds.stock + '/quote',
-//   headers: {'X-Starfighter-Authorization': creds.apiToken}
-// };
+var marketOptions = {
+  host: creds.baseUrl,
+  path: '/ob/api/venues/' + creds.venue + '/stocks/' + creds.stock + '/quote',
+  headers: {'X-Starfighter-Authorization': creds.apiToken}
+};
 
 // Return a promise for a buy command given options and an order.
 var buy = function(options, order) {
@@ -47,39 +48,90 @@ var buy = function(options, order) {
   });
 };
 
-function buyMore(i, quantity) {
-  var order = {
+// Return a promise for a buy command given options and an order.
+var peek = function(options) {
+  return new Promise(function(resolve, reject) {
+    var req = http.request(options, function(response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      response.on('end', function () {
+        var data = JSON.parse(str);
+        if (data.ok === false) {
+          reject(data.error);
+        }
+        resolve(data); // resolve the promise with a JSON object of the data
+      }); 
+    });
+
+    req.on('error', function(e) {
+      reject(e.message); // hit an error; reject the promise and clean up
+    }); 
+
+    // This is the data we are posting; it needs to be a string or a buffer.
+    req.end();
+  });
+};
+
+// Buy in quantities of half the askSize while the asking price is at or below
+// our goal price until i is 0. Wait if the asking price is too high or the
+// askSize is too small.
+function buyMore(i) {
+  var buyOrder = {
     'account': creds.account,
     'venue': creds.venue,
     'symbol': creds.stock,
-    'price': 25000,
-    'qty': quantity,
+    'price': 0,
+    'qty': 0,
     'direction': "buy",
     'orderType': "limit"
   };
 
-  buy(buyOptions, order).then(function(res) {
-    if (res.totalFilled < orderSize && filledToPresent <= goal - 200) {
-      console.log("Odd, you didn't fill as many as you expected to.");
-      return;
+  if (i < 0) {
+    console.log('Error: Exceeded goal of ' + goal + ' by ' + Math.abs(i));
+  }
+
+  if (i === 0) {
+    console.log('Finished!');
+    return;
+  }
+
+  peek(marketOptions).then(function(res) { 
+    if (res.ask === undefined) {
+      console.log('res.ask was undefined');
+      return Promise.resolve({res: i});
     }
 
-    var filledToPresent = i + res.totalFilled;
-    if (filledToPresent == goal) {
-      // Finished!
-      return;
-    } else if (filledToPresent <= goal - 200) {
-      // Keep buying 200.
-      sleep.usleep(1000);
-      buyMore(filledToPresent, orderSize);
-    } else {
-      // Only buy enough to get us to the limit.
-      sleep.usleep(1000);
-      buyMore(filledToPresent, goal - filledToPresent);
+    if (res.askSize < quantity) {
+      console.log('Only ' + res.askSize + ' shares left, but we wanted ' + quantity + '.');
+      return Promise.resolve({res: i});
     }
-  }).catch(function(err) {
-    console.log('problem: ' + err);
+
+    if (res.ask > price) {
+      console.log('Price of ' + res.ask + ' is too high. Wanted a price of ' + price);
+      return Promise.resolve({res: i});
+    }
+
+    // Price is below what we're looking for and stocks are available... Buy!
+    var bidSize = quantity;
+    if (bidSize > i) {
+      // Only bid for as much as we need.
+      bidSize = i;
+    }
+
+    console.log('Buying ' + bidSize + ' shares at ' + res.ask + ' when ' + res.askSize + ' are available.');
+    buyOrder.price = res.ask;
+    buyOrder.qty = bidSize;
+    return buy(buyOptions, buyOrder);
+  })
+  .then(function(res) {
+    setTimeout(function() {buyMore(i - res.totalFilled);}, 400);
+  })
+  .catch(function(err) {
+    console.log('Error: ' + err);
   });
 }
 
-buyMore(0, orderSize);
+buyMore(goal);
