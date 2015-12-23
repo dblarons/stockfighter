b90d7d955e680a020302760f92009f5a56b4fa00
Aaron Smith <asmith1810@gmail.com>
2015-12-22 18:18:08 -0800

    Use promises to isolate some boilerplate code.

--- aaron/app.js
+++ aaron/app.js
@@ -1,7 +1,9 @@
 var http = require('https');
 var creds = require('./exports.js');
 var sleep = require('sleep');
+var Promise = require('promise');
 
+var goal = 100000;
 var orderSize = 200;
 
 var buyOptions = {
@@ -17,61 +19,67 @@ var buyOptions = {
 //   headers: {'X-Starfighter-Authorization': creds.apiToken}
 // };
 
-function buyMore(i, quantity) {
-  var req = http.request(buyOptions, function(response) {
-    var str = '';
-    response.on('data', function (chunk) {
-      str += chunk;
+// Return a promise for a buy command given options and an order.
+var buy = function(options, order) {
+  return new Promise(function(resolve, reject) {
+    var req = http.request(options, function(response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      response.on('end', function () {
+        var data = JSON.parse(str);
+        if (data.ok === false) {
+          reject(data.error);
+        }
+        resolve(data); // resolve the promise with a JSON object of the data
+      }); 
     });
 
-    response.on('end', function () {
-      var data = JSON.parse(str);
-      var lastFilledSize = data.fills.reduce(function(prev, curr) {
-        return prev + curr.quantity;
-      }, 0);
-
-      if (lastFilledSize < orderSize && filledToPresent <= 100000 - 200) {
-        console.log("Odd, you didn't fill as many as you expected to.");
-        return;
-      }
-
-      var filledToPresent = i + data.totalFilled;
-      if (filledToPresent == 100000) {
-        // Finished!
-        return;
-      } else if (filledToPresent <= 100000 - 200) {
-        // Keep buying 200.
-        sleep.usleep(1000);
-        buyMore(filledToPresent, orderSize);
-      } else {
-        // Only buy enough to get us to the limit.
-        sleep.usleep(1000);
-        buyMore(filledToPresent, 100000 - filledToPresent);
-      }
+    req.on('error', function(e) {
+      reject(e.message); // hit an error; reject the promise and clean up
     }); 
-  });
 
-  req.on('error', function(e) {
-    console.log('problem: ' + e.message);
+    // This is the data we are posting; it needs to be a string or a buffer.
+    req.write(JSON.stringify(order));
+    req.end();
   });
+};
 
+function buyMore(i, quantity) {
   var order = {
     'account': creds.account,
     'venue': creds.venue,
     'symbol': creds.stock,
     'price': 25000,
-    'qty': orderSize,
+    'qty': quantity,
     'direction': "buy",
     'orderType': "limit"
   };
 
-  // This is the data we are posting, it needs to be a string or a buffer
-  req.write(JSON.stringify(order));
-  req.end();  
+  buy(buyOptions, order).then(function(res) {
+    if (res.totalFilled < orderSize && filledToPresent <= goal - 200) {
+      console.log("Odd, you didn't fill as many as you expected to.");
+      return;
+    }
+
+    var filledToPresent = i + res.totalFilled;
+    if (filledToPresent == goal) {
+      // Finished!
+      return;
+    } else if (filledToPresent <= goal - 200) {
+      // Keep buying 200.
+      sleep.usleep(1000);
+      buyMore(filledToPresent, orderSize);
+    } else {
+      // Only buy enough to get us to the limit.
+      sleep.usleep(1000);
+      buyMore(filledToPresent, goal - filledToPresent);
+    }
+  }).catch(function(err) {
+    console.log('problem: ' + err);
+  });
 }
 
 buyMore(0, orderSize);
-
-
-
-
