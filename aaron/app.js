bc17e97d3d6321a270c8c47f3bcc860fd235527f
Aaron Smith <asmith1810@gmail.com>
2015-12-24 21:34:08 -0800

    Use the new API wrapper in level2 code.

--- aaron/app.js
+++ aaron/app.js
@@ -9,97 +9,10 @@ var price = 9510;
 var quantity = 500;
 var sellQuantity = 25;
 
-var orderOptions = {
-  host: creds.baseUrl,
-  path: '/ob/api/venues/' + creds.venue + '/stocks/' + creds.stock + '/orders',
-  method: 'POST',
-  headers: {'X-Starfighter-Authorization': creds.apiToken}
-};
-
-var marketOptions = {
-  host: creds.baseUrl,
-  path: '/ob/api/venues/' + creds.venue + '/stocks/' + creds.stock + '/quote',
-  headers: {'X-Starfighter-Authorization': creds.apiToken}
-};
-
-// Return a promise for a buy command given options and an order.
-var placeOrder = function(options, order) {
-  return new Promise(function(resolve, reject) {
-    var req = http.request(options, function(response) {
-      var str = '';
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      response.on('end', function () {
-        var data = JSON.parse(str);
-        if (data.ok === false) {
-          reject(data.error);
-        }
-        resolve(data); // resolve the promise with a JSON object of the data
-      }); 
-    });
-
-    req.on('error', function(e) {
-      reject(e.message); // hit an error; reject the promise and clean up
-    }); 
-
-    // This is the data we are posting; it needs to be a string or a buffer.
-    req.write(JSON.stringify(order));
-    req.end();
-  });
-};
-
-// Return a promise for a buy command given options and an order.
-var peek = function(options) {
-  return new Promise(function(resolve, reject) {
-    var req = http.request(options, function(response) {
-      var str = '';
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      response.on('end', function () {
-        var data = JSON.parse(str);
-        if (data.ok === false) {
-          reject(data.error);
-        }
-        resolve(data); // resolve the promise with a JSON object of the data
-      }); 
-    });
-
-    req.on('error', function(e) {
-      reject(e.message); // hit an error; reject the promise and clean up
-    }); 
-
-    req.end();
-  });
-};
-
 // Buy in quantities of a size defined by the quantity constant while the
 // asking price is at or below our goal price until i is 0. Wait if the asking
 // price is too high or the askSize is too small.
 function buyMore(i) {
-  var buyOrder = {
-    'account': creds.account,
-    'venue': creds.venue,
-    'symbol': creds.stock,
-    'price': 0,
-    'qty': 0,
-    'direction': "buy",
-    'orderType': "limit"
-  };
-
-  var sellOrder = {
-    'account': creds.account,
-    'venue': creds.venue,
-    'symbol': creds.stock,
-    'price': 0,
-    'qty': 0,
-    'direction': "sell",
-    'orderType': "limit"
-  };
-
   if (i < 0) {
     console.log('Error: Exceeded goal of ' + goal + ' by ' + Math.abs(i));
   }
@@ -133,14 +46,10 @@ function buyMore(i) {
     }
 
     console.log('Buying ' + bidSize + ' shares at ' + res.ask + ' when ' + res.askSize + ' are available.');
-    buyOrder.price = res.ask;
-    buyOrder.qty = bidSize;
-    sellOrder.price = res.bid;
-    sellOrder.qty = sellQuantity;
-    return bid(creds.venueId, creds.stockId, price, quantity, 'limit')
-    .then(ask(...))
-    .then(ask(...))
-    .then(ask(...));
+    return bid(creds.venueId, creds.stockId, res.ask, bidSize, 'limit')
+    .then(ask(creds.venueId, creds.stockId, res.bid, sellQuantity, 'limit'))
+    .then(ask(creds.venueId, creds.stockId, res.bid, sellQuantity, 'limit'))
+    .then(ask(creds.venueId, creds.stockId, res.bid, sellQuantity, 'limit'));
   })
   .then(res => {
     setTimeout(function() {buyMore(i - res.totalFilled + sellQuantity * 3);}, 400);
