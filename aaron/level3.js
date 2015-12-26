ae781d3d2fbb04dc6a5c0aa4f00245edac401268
Aaron Smith <asmith1810@gmail.com>
2015-12-26 14:16:15 -0800

    Outline for level3

--- aaron/level3.js
+++ aaron/level3.js
@@ -2,17 +2,41 @@
 
 var creds = require('./exports.js');
 var API = require('../api.js').API;
+var Immutable = require('immutable');
 
 /*
  * Conditions for when to buy
+ *  - Buy as long as:
+ *    price < cheapestSellPrice && position + depth < 1000
  * Keep track of what has been bought
+ *  - Put order Id's along with the current status in a data structure. Update
+ *    it periodically and remove when the order is filled.
+ *  - TODO: When to cancel and repost a buy order.
  * Conditions for when to sell
+ *  - Use a MinHeap and constantly post the minimum element to the market.
+ *  - After purchasing a stock, place into the MinHeap at a fixed price more
+ *    than it was bought for.
+ *  - TODO: When to cancel and repost a sell order.
  * Keep track of what has been posted to sell
- * Keep track of our positions
- * Keep track of our cash value
- * Keep track of our net value
+ *  - Put order Id's along with the current status in a data structure. Update
+ *    it periodically and remove when the order is filled.
+ * Keep track of our position (inversely related to cash value)
+ *  - When a stock is bought, add it to our position; when sold, subtract.
+ * Keep track of our cash value (inversely related to position)
+ *  - When a stock is sold, add to cash value; when bought, subtract.
+ * Keep track of our net asset value (NAV)
+ *  - Calculate difference between amount of cash spent and the current value
+ *    of the stocks that are held.
+ *  - Stop once net > 10000
  */
 
-var position = {
+var State = Immutable.Map({
+  // Summary of holdings.
+  cash: 0,
+  position: 0,
+  nav: 0,
 
-}
+  openBids: Immutable.List(), // open buy orders
+  openAsks: Immutable.List(), // open sell orders
+  
+});
